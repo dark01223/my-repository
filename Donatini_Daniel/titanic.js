be394d55f81c223b1c4819014e4e83c589218710
dark01223 <138132975+dark01223@users.noreply.github.com>
2024-06-06 10:23:45 +0200

    Titanic
    Titanic Site

---
+++ Donatini_Daniel/titanic.js
@@ -0,0 +1,65 @@
+async function populate(){
+
+    const requestURL = "https://dark01223.github.io/my-repository/csvjson.json"; // gli viene assegnato un link 
+
+    const request = new Request(requestURL) // crea un nuovo oggetto
+
+    const response = await fetch(request); // 
+    
+    const titanicPeople = await response.json(); //
+
+    populatePeople(titanicPeople);
+
+
+}
+
+function populatePeople(obj){ 
+    
+    for(i = 0; i <= obj.length; i++){  
+ 
+        addElement(obj[i]); // richiamo la funzione addElement passando il vettore I-esimo
+    
+    }
+}
+
+function addElement(obj){ 
+
+    const newDiv = document.createElement('div');  // crea un elemento DIV che sarà il contenitore
+    const newH2 = document.createElement('h2');
+    const newUl = document.createElement('ul');
+    const newLi1 = document.createElement('li');
+    const newLi2 = document.createElement('li');
+    const newLi3 = document.createElement('li');
+
+    newDiv.setAttribute("id", "card");  // assegno la card all'attributo ID per assegnarlo allo Style.css
+
+    newH2.textContent = obj.Name;  // assegna i valori ai rispettivi elementi creati
+    newLi1.textContent = `Ha : ${obj.Age} anni`; 
+  
+
+    if(obj.Survived == 0){   // funzione IF che controlla il valore di Survived e assegna il rispettivo valore
+
+        newLi2.textContent = "Non sopravvissuto";
+    }else{
+
+        newLi2.textContent = "Sopravvissuto";
+    };
+
+    if(obj.Sex == "male"){   // funzione IF che controlla il valore di Sex e assegna il rispettivo valore
+
+        newLi3.textContent = "Maschio";
+    }else{
+
+        newLi3.textContent = "Femmina";
+    }
+
+
+    newDiv.append(newH2, newUl);  // appen serve per attaccare H2 e UL al nuovo DIV creato
+    newUl.appendChild(newLi1);  
+    newUl.appendChild(newLi2);
+    newUl.appendChild(newLi3);
+    document.body.append(newDiv);  // attacca il DIV creato al Body
+
+}
+
+populate();
